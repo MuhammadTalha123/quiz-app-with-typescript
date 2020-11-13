5f46ceb6a971b87da2d67151b3307c12510e91b4
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-11-13 05:45:58 +0500

    putting export data in index.tsx

---
+++ src/components/index.tsx
@@ -0,0 +1,7 @@
+export type { AnswerObject } from "./AnswerType";
+export type { Question } from "./QuestionType";
+export { Difficulty } from "./DifficultyType";
+export { QuestionCard } from "./QuestionCard";
+export type { QuestionState } from "./DifficultyType";
+export type { Props } from "./PropsType";
+export {LoadingDiv} from "./loading";
